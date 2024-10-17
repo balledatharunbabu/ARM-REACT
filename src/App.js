82fb2568975abcbe44daffe8eb354982f7d77048
balledatharunbabu <tharuntej94414@gmail.com>
2024-10-17 19:14:24 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home'
+
+import ButtonAppBar from './Components/appbar';
+
+import ViewScenerio from './Pages/ViewScenerio'
+
+import ViewScenerioreponse from './Pages/Viewscenrioresponse'
+
+import Exceptionscenerio from './Pages/ExceptionScenerio'
+
+import ScenarioCreation from './Pages/Createscenerio'
+
+import Auditscenerio from './Pages/Auditscenerio'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return(
+    <>
+
+    <BrowserRouter>
+
+        <Routes>
+     
+          <Route path="/" element={<Home></Home>}></Route>
+
+          <Route path="/scenario/view" element={<ViewScenerio></ViewScenerio>}></Route>
+
+          <Route path="/scenario/viewscenerio" element={<ViewScenerioreponse></ViewScenerioreponse>}></Route>
+
+          
+          <Route path="/scenario/createscenerio" element={<ScenarioCreation/>}></Route>
+
+          <Route path="/scenario/exception" element={<Exceptionscenerio></Exceptionscenerio>}></Route>
+
+          <Route path="/scenario/audit" element={<Auditscenerio></Auditscenerio>}></Route>
+          
+          <Route path="/scenario/replay" element={<Auditscenerio></Auditscenerio>}></Route>
+
+        </Routes>
+      
+    </BrowserRouter>    
+       
+        
+    </>
+
+  ) 
 }
 
 export default App;
